1e6b528e3db81b88ae962a04e15a826cdd768aff
Min Maung <minmaung0307@gmail.com>
2022-09-25 19:47:23 -0400

    Post Model  created

--- models/index.js
+++ models/index.js
@@ -1,3 +1,7 @@
-const User = require('./User');
+const User = require("./User");
+const Post = require("./Post");
 
-module.exports = { User };
+module.exports = { User, Post };
+
+// const User = require('./User');
+// module.exports = { User };
